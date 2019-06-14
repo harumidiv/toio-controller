e7d86e7317055bfe0d483c76046847a2c66a7f11
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-06-14 22:25:02 +0900

    feat: Change rootviewController

---
+++ ToioController/Presentation/ControlViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ControlViewController.swift
+//  ToioController
+//
+//  Created by 佐川晴海 on 2019/06/14.
+//  Copyright © 2019 佐川晴海. All rights reserved.
+//
+
+import UIKit
+
+class ControlViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "toio controller"
+    }
+
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+     }
+     */
+}
