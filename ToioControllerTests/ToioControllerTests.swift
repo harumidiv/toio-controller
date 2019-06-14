e662cda7c7cf35e3843c4203d85a00687635af59
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-06-14 22:18:11 +0900

    feat: Add swiftlint and swiftformat runscript

--- ToioControllerTests/ToioControllerTests.swift
+++ ToioControllerTests/ToioControllerTests.swift
@@ -6,11 +6,10 @@
 //  Copyright © 2019 佐川晴海. All rights reserved.
 //
 
-import XCTest
 @testable import ToioController
+import XCTest
 
 class ToioControllerTests: XCTestCase {
-
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -26,9 +25,8 @@ class ToioControllerTests: XCTestCase {
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
