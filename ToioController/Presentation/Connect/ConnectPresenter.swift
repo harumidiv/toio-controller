92b3b067a7828bd7237e1cbb044b946a0ae01630
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-06-15 13:48:54 +0900

    feat: Add checkbattery method

--- ToioController/Presentation/Connect/ConnectPresenter.swift
+++ ToioController/Presentation/Connect/ConnectPresenter.swift
@@ -6,18 +6,60 @@
 //  Copyright © 2019 佐川晴海. All rights reserved.
 //
 
-import Foundation
 import RxSwift
+import RxBluetoothKit
 
 protocol ConnectPresenter: AnyObject {
     func checkPhoneState()
     func loadDevice()
 }
 
-protocol ConnectPresenterOutput: AnyObject {}
+protocol ConnectPresenterOutput: AnyObject {
+    func showBatteryError()
+    func showBluetoothError()
+    func showDevice()
+
+}
 
 class ConnectPresenterImpl: ConnectPresenter {
-    func checkPhoneState() {}
+    
+    private let phoneDeviceUseCase: PhoneDeviceUsecase = OtherInjector.container.resolve(PhoneDeviceUsecase.self)!
+    
+    private weak var output: ConnectPresenterOutput?
+    
+    init(output: ConnectPresenterOutput) {
+        self.output = output
+    }
+    
+    // MAKR: - Public methods
+    func checkPhoneState() {
+        phoneDeviceUseCase.getBattery()
+            .take(1)
+            .observeOn(MainScheduler.instance)
+            .flatMap { [weak self] (battery) -> Observable<BluetoothState> in
+                guard let self = self else {
+                    return Observable.empty()
+                }
+                if battery < 20 {
+                //TODO self.output.バッテリー足りない時
+                    return Observable.empty()
+                }
+                return self.phoneDeviceUseCase.getBluetoothState()
+            }
+            .subscribe(onNext:{[weak self] state in
+                guard let self = self else {
+                    return
+                }
+                switch state {
+                case .poweredOn:
+                    self.output?.showDevice()
+                default:
+                    self.output?.showBluetoothError()
+                }
+            })
+    }
 
-    func loadDevice() {}
+    func loadDevice() {
+        
+    }
 }
