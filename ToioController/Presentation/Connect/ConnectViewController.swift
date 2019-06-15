b20a3bd9af181ee8d4236a7d40fcebff12f34b11
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-06-15 12:07:40 +0900

    feat: Add connectviewController

---
+++ ToioController/Presentation/Connect/ConnectViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConnectViewController.swift
+//  ToioController
+//
+//  Created by 佐川晴海 on 2019/06/15.
+//  Copyright © 2019 佐川晴海. All rights reserved.
+//
+
+import UIKit
+
+class ConnectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
